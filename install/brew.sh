dfaffbfe57b8066914bdff6f16bd9b17adbd0839
Tony Abboud <tdabboud@hotmail.com>
2016-07-24 09:39:30 -0400

    add more packages to install

--- install/brew.sh
+++ install/brew.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 # cli tools
-brew install ack
+brew install the_silver_searcher
 brew install tree
 brew install wget
 
 # development tools
 brew install git
-brew install tmux
+brew install tig
+brew install tmux --HEAD
 brew install zsh
+brew cask install iterm2
+
+brew cask install dropbox
 
 exit 0
