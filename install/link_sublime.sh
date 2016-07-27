61a2faf2cda603fac173eaec01df6d2beb3e7228
Tony Abboud <tdabboud@hotmail.com>
2016-07-26 20:55:37 -0400

    add support to symlink sublime text settings
        This does not currently override a file if it exists

--- install/link_sublime.sh
+++ install/link_sublime.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # link sublime text into osx
 
-SUBLIME=$HOME/.dotfiles/sublime
-SUBLIME_DIR="${HOME}/Library/Application Support/Sublime Text 3/Packages"
+SUBLIME_DOTFILES=$HOME/.dotfiles/sublime
+SUBLIME_DIR="${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
 
 echo -e "\nCreating Sublime Symlinks"
 echo "=============================="
-linkables=$( find -H "$SUBLIME" -maxdepth 3 -name '*.symlink' )
-for file in $linkables ; do
-    target="$SUBLIME_DIR/.$( basename $file ".symlink" )"
-    if [ -e $target ]; then
+
+find "$SUBLIME_DIR" -iname "*.sublime-*" | while read f
+do
+    target="$SUBLIME_DIR"/$(basename "$f")
+    if [ -e "$target" ]; then
         echo "~${target#$HOME} already exists... Skipping."
     else
-        echo "Creating symlink for $file"
-        ln -s $file $target
+        echo "Creating symlink for $f"
+        ln -s "$f" "$target"
     fi
 done
