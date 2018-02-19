faed0371031afbe8fa62cbcf2cc18412408fd042
tdabboud <tdabboud@hotmail.com>
2018-02-18 23:05:08 -0500

    modularize install script

--- install.sh
+++ install.sh
@@ -5,29 +5,84 @@ COLOR_GREEN='\033[0;32m'
 COLOR_NONE='\033[0m'
 COLOR_YELLOW='\033[1;33m'
 
-echo "Initializing submodule(s)..."
-git submodule update --init --recursive
+OS="$(uname)"
 
 
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Installing stow..."
-    bash install/brew.sh install-stow
+function bootstrap() {
+    echo "Bootstrapping the system"
 
+    echo "Updating submodule(s)..."
+    git submodule update --init --recursive
+
+    if [ "$OS" == "Darwin" ]; then
+        echo "Brewing Everything..."
+        bash install/brew.sh everything
+
+        echo "Updating OSX settings..."
+        bash install/osx.sh
+
+        echo "Link in the iTerm font"
+        source install/link_iterm_fonts.sh
+    elif [ "$OS" == "Linux" ]; then
+        echo "nothing to do"
+    else
+        echo "Unsupported OS type!"
+        exit 1
+    fi
+}
+
+function dotfiles() {
     echo "Linking all dotfiles..."
-    bash install/run_stow.sh osx
+    if [ "$OS" == "Darwin" ]; then
+        bash install/brew.sh install-stow
+        bash install/run_stow.sh osx
+    elif [ "$OS" == "Linux" ]; then
+        bash install/run_stow.sh linux
+    else
+        echo "Unsupported OS type!"
+        exit 1
+    fi
+}
 
-    echo "Brewing Everything..."
-    bash install/brew.sh everything
+function all() {
+    dotfiles
+    bootstrap
+}
 
-    echo "Updating OSX settings..."
-    bash install/osx.sh
+function usage() {
+cat<<EOD
+Usage:
+    bootstrap   [options] -- Just install software packages
+    dotfiles    [options] -- Just link all dotfiles
+    all         [options] -- Install software and link dotfiles
+EOD
+  exit 1
+}
 
-    echo "Link in the iTerm font"
-    source install/link_iterm_fonts.sh
-elif [ "$(uname)" == "Linux" ]; then
-    echo "Linking all dotfiles..."
-    bash install/run_stow.sh linux
+if [[ "$#" == "0" ]]; then
+  usage
 fi
 
-echo -e "${COLOR_GREEN}✔ All Done!${COLOR_NONE}"
+command="$1"
+if [[ "$command" == "" ]]; then
+  echo "first argument must be a command"
+  usage
+fi
 
+case "$command" in 
+  all)
+    all
+    ;;
+  dotfiles)
+    dotfiles
+    ;;
+  bootstrap)
+    bootstrap
+    ;;
+  *)
+    usage
+    ;;
+esac
+
+echo -e "${COLOR_GREEN}✔ All Done!${COLOR_NONE}"
+exit 0
