bc320b2eb8bb31cdfd4289183beeb145b4a64cd6
Tony Abboud <tdabboud@hotmail.com>
2021-11-18 10:51:15 -0500

    Add fzf scripts (#36)

---
+++ shell/fzf-scripts.sh
@@ -0,0 +1,41 @@
+# Functions that leverage FZF
+
+# fuzzy grep open via ag
+vg() {
+  local file
+
+  file="$(ag --nobreak --noheading $@ | fzf -0 -1 | awk -F: '{print $1}')"
+
+  if [[ -n $file ]]
+  then
+     vim $file
+  fi
+}
+
+# Cd to a Go repo via fzf.
+#
+# Finds all repositories under $GOPATH/src, strips out only the org/repo name and provides selection through fzf.
+# Ctrl-c can be used to cancel the selection.
+gopkg() {
+    # pipe to awk to only retrieve the org/repo for less noise when listing
+    # This only works because the CDPATH contains the search paths below
+    local gorepo
+
+    gorepo="$(find $GOPATH/src -type d -maxdepth 3 -mindepth 1 | awk -F/ '{print $(NF-2)"/"$(NF-1)"/"$(NF)}' | fzf -0 -1)"
+    if [[ -n $gorepo ]]; then
+        cd $GOPATH/src/$gorepo
+    fi
+}
+
+# Create a new tmux session or switch to an existing one.
+#
+# Commands:
+# tm: select an open tmux session via fzf
+# tm <session-name>: Attaches to <session-name> or creates it
+tm() {
+  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+  if [ $1 ]; then
+    tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+  fi
+  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+}
