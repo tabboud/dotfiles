5d977b052c078c52d15c84fa285424896ca6c8f8
tdabboud <tdabboud@hotmail.com>
2022-04-25 23:02:43 -0400

    Add telescope

--- vim/.config/nvim/lua/plugins/init.lua
+++ vim/.config/nvim/lua/plugins/init.lua
@@ -53,4 +53,8 @@ Plug 'neovim/nvim-lspconfig'
 -- Use fzf with nvim-lspconfig (equivalent to using telescope)
 Plug('ojroques/nvim-lspfuzzy', { ['branch'] = 'main'})
 
+-- Telescope
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+
 call('plug#end')
