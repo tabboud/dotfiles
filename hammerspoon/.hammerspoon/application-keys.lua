8f55afa163e7038f74e0c0b985ff715321e62349
Tony Abboud <tabboud@palantir.com>
2022-05-18 14:36:28 -0400

    Update hammerspoon settings

---
+++ hammerspoon/.hammerspoon/application-keys.lua
@@ -0,0 +1,85 @@
+-- Application key mappings
+-- This file sets up the various hotkeys to use to
+-- quickly switch between applications.
+--
+local application = require("hs.application")
+local hotkey = require("hs.hotkey")
+local log = require("hs.logger").new("Application Keys", "info")
+
+shortcutKey = {"cmd", "ctrl"}
+local shortcuts = {
+  ["normal"] = {},
+}
+
+Keys = {
+  ["specialTriggers"] = {},
+  ["triggers"] = {
+    ["iTerm"] = {{shortcutKey, "t"}},
+    ["Google Chrome"] = {{shortcutKey, "i"}},
+    ["Slack"] = {{shortcutKey, "s"}},
+    ["Messages"] = {{shortcutKey, "m"}},
+    ["Microsoft Outlook"] = {{shortcutKey, "o"}},
+    ["GoLand"] = {{shortcutKey, "g"}},
+    ["IntelliJ IDEA Community Edition"] = {{shortcutKey, "0"}},
+    ["Quip"] = {{shortcutKey, "q"}},
+  },
+}
+
+function Keys.keyFor(name)
+  local keys = Keys.triggers[name]
+  if not keys then
+    keys = Keys.specialTriggers[name]
+  end
+
+  return keys
+end
+
+function Keys.bindKeyFor(appName, fn)
+  keys = Keys.keyFor(appName)
+  normalKeys = keys[1]
+  shortcuts["normal"][appName] = hotkey.new(normalKeys[1], normalKeys[2], fn)
+end
+
+function Keys.deactivate()
+  for _, keys in pairs(shortcuts) do
+    for __, k in pairs(keys) do
+      k:disable()
+    end
+  end
+end
+
+function Keys.activate()
+  keys = shortcuts["normal"]
+  for _, k in pairs(keys) do
+      log.i(k)
+    k:enable()
+  end
+end
+
+-- Setup the applications to bind to based on the configured key mapping/trigger.
+for applicationName, _ in pairs(Keys.triggers) do
+  Keys.bindKeyFor(applicationName, function()
+    application.launchOrFocus(applicationName)
+  end)
+end
+
+-- Returns a string that contains all currently mapped keys
+-- for use in an alert.
+function Keys.getAlertMapping(name)
+    local allKeys = ""
+
+    -- TODO(): sort the table for consistent output
+    for appName, mappings in pairs(Keys.triggers) do
+        normalKeys = mappings[1]
+        appKey = normalKeys[2]
+        local modKeys = ""
+        for k, v in pairs(normalKeys[1]) do
+            modKeys = modKeys .. v .. "+"
+        end
+
+        allKeys = allKeys .. string.format("%-32s: %s", appName, modKeys .. appKey) .. "\n"
+    end
+  return allKeys
+end
+
+return Keys
--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,49 +1,33 @@
 -- Hammerspoon Configs
 
 -- Modules
-require("hs.application")
-require("hs.logger")
-require("hs.screen")
-require("hs.window")
-require("hs.alert")
+local alert = require("hs.alert")
+local applicationKeys = require("application-keys")
 
--- ctrlTap module
--- require("ctrltap")
-
-local keys = require "keys"
-require "triggers"
-
--- Logging
-local log = hs.logger.new("My Config", "info")
-
--- Add Fast Application Switching
-keys.deactivateKeys()
-keys.activateKeys()
-
--- Reload config
+-- Shortcut to reload config
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function() hs.reload() end)
 
--- Show date time and battery
+-- Show date, time, and battery life
 hs.hotkey.bind({"cmd", "ctrl"}, "D", function()
   local seconds = 3
   local message = os.date("%I:%M%p") .. "\n" .. os.date("%a %b %d") .. "\nBattery: " ..
      hs.battery.percentage() .. "%"
-  hs.alert.show(message, seconds)
+  alert.show(message, seconds)
 end)
-
--- Install / Use Spoons
--- hs.loadSpoon("SpoonInstall")
--- spoon.SpoonInstall:andUse("WindowHalfsAndThirds",
---                {
---                  config = {
---                    use_frame_correctness = true
---                  },
---                  hotkeys = 'default',
---                }
--- )
-
 
 -- Load ControlEscape which maps capslock to ESC when tapped and ctrl when held
 hs.loadSpoon('ControlEscape'):start()
+
+-- Fast Application Switching
+applicationKeys.deactivate()
+applicationKeys.activate()
+
+
+-- List the current application key mappings in an alert window
+hs.hotkey.bind({"cmd", "ctrl"}, "M", function()
+  local seconds = 5
+  local allApplicationKeys = applicationKeys.getAlertMapping()
+  alert.show(allApplicationKeys, seconds)
+end)
 
 hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()
