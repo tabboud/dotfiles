a49f472a7e6d378e351132a38f2374e6e2d2b40f
Tony Abboud <tabboud@palantir.com>
2022-06-23 11:55:56 -0400

    Update lspconfig settings

--- vim/.config/nvim/lua/plugins/lspconfig.lua
+++ vim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,39 @@
 local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
+local vim_diag = require('vim.diagnostic')
+
+local M = {}
+
+M.diagnostic_config = {
+    update_in_insert = false,
+    underline = false,
+    signs = true,
+    severity_sort = true,
+    virtual_text = {
+        prefix = "",
+        spacing = 4,
+    },
+}
+
+M.icons = {
+    error = " ",
+    warn = " ",
+    hint = " ",
+    info = " ",
+}
+
+M.get_capabilities = function()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+            "documentation",
+            "detail",
+            "additionalTextEdits",
+        },
+    }
+    return cmp_nvim_lsp.update_capabilities(capabilities)
+end
 
 local on_attach = function(client, bufnr)
   vim.api.nvim_set_keymap("n", "<Leader>o", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", {noremap = true, silent = true})
@@ -22,51 +56,104 @@ local on_attach = function(client, bufnr)
     -- Setup highlight under cursor if the server supports it
     -- This replaces any usage of treesitter
    if client.resolved_capabilities.document_highlight then
-       -- vim.api.nvim_exec([[
-       --     if (&background == "light")
-       --         " Match intellij hover bg color
-       --         hi LspReferenceRead cterm=bold ctermbg=red guibg=#E4E4FF
-       --         hi LspReferenceText cterm=bold ctermbg=red guibg=#E4E4FF
-       --         hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-       --     else
-       --         hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-       --         hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-       --         hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-       --     endif
-       --   augroup lsp_document_highlight
-       --     autocmd!
-       --     autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-       --     autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-       --   augroup END
-       -- ]], false)
+        vim.api.nvim_create_autocmd("CursorHold", {
+            buffer = bufnr,
+            command = "lua vim.lsp.buf.document_highlight()",
+        })
+        vim.api.nvim_create_autocmd("CursorMoved", {
+            buffer = bufnr,
+            command = "lua vim.lsp.buf.clear_references()",
+        })
+   end
+
+   -- Handle document formatting on buffer write
+   if client.resolved_capabilities.document_formatting then
+       vim.api.nvim_create_autocmd("BufWritePre", {
+           buffer = bufnr,
+           command = "lua vim.lsp.buf.formatting_seq_sync()",
+       })
    end
 end
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    virtual_text = false,
-    signs = true,
-    update_in_insert = false,
-  }
-)
+-- Custom publishDiagnostics event handler
+vim.lsp.handlers["textDocument/publishDiagnostics"] = function(_, result, ctx)
+    local uri = result.uri
+    local bufnr = vim.uri_to_bufnr(uri)
+    if not bufnr then
+        return
+    end
 
-vim.fn.sign_define("LspDiagnosticsSignError", {text="", texthl="LspDiagnosticsSignError", linehl="", numhl=""})
-vim.fn.sign_define("LspDiagnosticsSignWarning", {text="", texthl="LspDiagnosticsSignWarning", linehl="", numhl=""})
-vim.fn.sign_define("LspDiagnosticsSignInformation", {text="", texthl="LspDiagnosticsSignInformation", linehl="", numhl=""})
-vim.fn.sign_define("LspDiagnosticsSignHint", {text="ﯦ", texthl="LspDiagnosticsSignHint", linehl="", numhl=""})
+    local diagnostics = result.diagnostics
+    for i, diagnostic in ipairs(diagnostics) do
+        local rng = diagnostic.range
+        diagnostics[i].lnum = rng["start"].line
+        diagnostics[i].end_lnum = rng["end"].line
+        diagnostics[i].col = rng["start"].character
+        diagnostics[i].end_col = rng["end"].character
+    end
 
-vim.fn.sign_define("DiagnosticSignError", {text="", texthl="DiagnosticSignError", linehl="", numhl=""})
-vim.fn.sign_define("DiagnosticSignWarn", {text="", texthl="DiagnosticSignWarn", linehl="", numhl=""})
-vim.fn.sign_define("DiagnosticSignInfo", {text="", texthl="DiagnosticSignInfo", linehl="", numhl=""})
-vim.fn.sign_define("DiagnosticSignHint", {text="ﯦ", texthl="DiagnosticSignHint", linehl="", numhl=""})
+    local namespace = vim.lsp.diagnostic.get_namespace(ctx.client_id)
+    vim_diag.set(namespace, bufnr, diagnostics, M.diagnostic_config)
+    if not vim.api.nvim_buf_is_loaded(bufnr) then
+        return
+    end
+
+    vim_diag.show(namespace, bufnr, diagnostics, M.diagnostic_config)
+end
+
+-- Diagnostic sign mappings
+vim.fn.sign_define("LspDiagnosticsSignError",{
+    text=M.icons.error,
+    texthl="LspDiagnosticsSignError",
+    linehl="",
+    numhl="",
+})
+vim.fn.sign_define("LspDiagnosticsSignWarning", {
+    text=M.icons.warn,
+    texthl="LspDiagnosticsSignWarning",
+    linehl="",
+    numhl="",
+})
+vim.fn.sign_define("LspDiagnosticsSignInformation", {
+    text=M.icons.info,
+    texthl="LspDiagnosticsSignInformation",
+    linehl="",
+    numhl="",
+})
+vim.fn.sign_define("LspDiagnosticsSignHint", {
+    text=M.icons.hint,
+    texthl="LspDiagnosticsSignHint",
+    linehl="",
+    numhl="",
+})
+
+vim.fn.sign_define("DiagnosticSignError", {
+    text = M.icons.error,
+    numhl = "DiagnosticSignError",
+})
+vim.fn.sign_define("DiagnosticSignWarn", {
+    text = M.icons.warn,
+    numhl = "DiagnosticSignWarn",
+})
+vim.fn.sign_define("DiagnosticSignHint", {
+    text = M.icons.hint,
+    numhl = "DiagnosticSignHint",
+})
+vim.fn.sign_define("DiagnosticSignInfo", {
+    text = M.icons.info,
+    numhl = "DiagnosticSignInfo",
+})
 
 --
 -- gopls setup
 -- Settings can be found here: https://github.com/golang/tools/blob/master/gopls/doc/settings.md
 lspconfig.gopls.setup{
-  on_attach = on_attach,
-  capabilities = capabilities,
+  filetypes = { "go", "gomod", },
+  on_attach = function(client, bufnr)
+    require "lsp_signature".on_attach()  -- Note: add in lsp client on-attach
+    on_attach(client, bufnr)
+  end,
+  capabilities = M.get_capabilities(),
   cmd = {
     "gopls",
   },
