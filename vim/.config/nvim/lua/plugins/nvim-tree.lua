4c2450d0d8cf300581af016c164c0408dbeaba3a
Tony Abboud <tabboud@palantir.com>
2022-06-23 12:00:12 -0400

    Update nvim-tree settings

--- vim/.config/nvim/lua/plugins/nvim-tree.lua
+++ vim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -9,7 +9,7 @@ nvimtree.setup({
     ignore_list = {}
   },
   git = {
-    enable = true,
+    enable = false,
     -- Don't hide .gitignore files. These will show up with this symbol ◌
     ignore = false,
     timeout = 400,
@@ -20,5 +20,65 @@ nvimtree.setup({
       -- Toggle this with the toggle_custom command ('U')
       custom = {'vendor'},
   },
+
+  -- From luavim
+    diagnostics = {
+        enable = true,
+        show_on_dirs = false,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
+    disable_netrw = true,
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        ignore_list = {},
+    },
+    renderer = {
+        indent_markers = {
+          enable = false,
+          icons = {
+            corner = "└ ",
+            edge = "│ ",
+            none = "  ",
+          },
+        },
+        icons = {
+          webdev_colors = false,
+          show = {
+            git = true,
+            folder = true,
+            file = true,
+            folder_arrow = true,
+          },
+          glyphs = {
+            default = "",
+            symlink = "",
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              deleted = "",
+              untracked = "U",
+              ignored = "◌",
+            },
+            folder = {
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+            },
+          },
+        },
+        highlight_git = false,
+        highlight_opened_files = "name",
+        root_folder_modifier = ":t",
+    },
 }
 )
