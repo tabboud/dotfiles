cbd9a26b03afeab626910651e439a92f561cbdcf
Tony Abboud <tabboud@palantir.com>
2022-06-23 11:58:15 -0400

    disable vim highlighting

--- vim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ vim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,10 +1,10 @@
-
 require 'nvim-treesitter.configs'.setup({
     -- list of available parsers
-    ensure_installed = { 'go', 'json', 'yaml', 'vim' },
+    ensure_installed = { 'go', 'json', 'yaml' },
 
     highlight = {
         -- false disables the entire extension
         enable = true,
+        disable = { 'vim' },
     },
 })
