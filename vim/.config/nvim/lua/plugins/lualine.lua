08b90d7b49ae2137c4a73d2f911fcdf7964de2ef
Tony Abboud <tdabboud@hotmail.com>
2022-10-23 10:05:47 -0700

    vim: use lualine built-in searchCount

--- vim/.config/nvim/lua/plugins/lualine.lua
+++ vim/.config/nvim/lua/plugins/lualine.lua
@@ -1,28 +1,3 @@
--- searchCount returns the index of the last-pattern search term along with the total search count.
--- The output will be as follows: '/searchTerm [current/total]'
--- To clear the last search pattern run 'let @/ = ""' (see :h last-pattern)
-local function searchCount()
-  local searchTerm = vim.fn.getreg("/")
-  local count = vim.fn.searchcount({ maxcount = 100, recompute = 1 })
-  if next(count) == nil or count.total == 0 then
-    return "[0/0]"
-  end
-
-  if count.incomplete == 1 then
-    return "[?/?]"
-  end
-  if count.incomplete == 2 then
-    if count.total > count.maxcount and count.current > count.maxcount then
-      return string.format("/%s [>%d/>%d]", searchTerm, count.current, count.total)
-    end
-    if count.total > count.maxcount then
-      return string.format("/%s [%d/>%d]", searchTerm, count.current, count.total)
-    end
-  end
-
-  return string.format("/%s [%d/%d]", searchTerm, count.current, count.total)
-end
-
 -- projectName returns the name of the containing directory (or project).
 local function projectName()
   return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
@@ -33,8 +8,10 @@ require('lualine').setup {
     theme = 'jellybeans'
   },
   sections = {
-    lualine_b = { projectName, 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { searchCount }
+    lualine_a = { projectName },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = {},
+    lualine_x = { 'searchCount' },
+    lualine_z = {},
   },
 }
