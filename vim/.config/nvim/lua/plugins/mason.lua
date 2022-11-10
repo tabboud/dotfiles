8f631aaee92f243e7ec83764a02d63cf9b5ecefb
Tony Abboud <tabboud@palantir.com>
2022-11-10 08:52:31 -0800

    vim: add mason providers to workaround vpn issues

--- vim/.config/nvim/lua/plugins/mason.lua
+++ vim/.config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,12 @@
 local M = {}
 
 local options = {
+  providers = {
+    -- Use client providers instead of registry-api due to SSL issues using a VPN
+    -- ref: https://github.com/williamboman/mason.nvim/issues/633
+    "mason.providers.client",
+    -- "mason.providers.registry-api" -- This is the default provider. You can still include it here if you want, as a fallback to the client provider.
+  },
   ui = {
     icons = {
       package_installed = "✓",
