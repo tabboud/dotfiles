013b9ef9e90720623d6a021d5cb88c3348f13645
Tony Abboud <tabboud@palantir.com>
2022-11-17 14:06:28 -0800

    hammerspoon: update terminal mappings

--- hammerspoon/.hammerspoon/application-keys.lua
+++ hammerspoon/.hammerspoon/application-keys.lua
@@ -6,7 +6,7 @@ local application = require("hs.application")
 local hotkey = require("hs.hotkey")
 local log = require("hs.logger").new("Application Keys", "info")
 
-shortcutKey = {"cmd", "ctrl"}
+shortcutKey = { "cmd", "ctrl" }
 local shortcuts = {
   ["normal"] = {},
 }
@@ -14,16 +14,17 @@ local shortcuts = {
 Keys = {
   ["specialTriggers"] = {},
   ["triggers"] = {
-    ["iTerm"] = {{shortcutKey, "t"}},
-    -- ["Alacritty"] = {{shortcutKey, "t"}},
-    ["Google Chrome"] = {{shortcutKey, "i"}},
-    ["Slack"] = {{shortcutKey, "s"}},
-    ["Messages"] = {{shortcutKey, "m"}},
-    ["Microsoft Outlook"] = {{shortcutKey, "o"}},
-    ["GoLand"] = {{shortcutKey, "g"}},
-    ["IntelliJ IDEA Community Edition"] = {{shortcutKey, "0"}},
-    ["Visual Studio Code"] = {{shortcutKey, "c"}},
-    ["Quip"] = {{shortcutKey, "q"}},
+    -- ["iTerm"] = {{shortcutKey, "t"}},
+    ["Alacritty"] = { { shortcutKey, "t" } },
+    -- ["kitty"] = { { shortcutKey, "t" } },
+    ["Google Chrome"] = { { shortcutKey, "i" } },
+    ["Slack"] = { { shortcutKey, "s" } },
+    ["Messages"] = { { shortcutKey, "m" } },
+    ["Microsoft Outlook"] = { { shortcutKey, "o" } },
+    ["GoLand"] = { { shortcutKey, "g" } },
+    ["IntelliJ IDEA Community Edition"] = { { shortcutKey, "0" } },
+    ["Visual Studio Code"] = { { shortcutKey, "c" } },
+    ["Quip"] = { { shortcutKey, "q" } },
   },
 }
 
@@ -53,7 +54,7 @@ end
 function Keys.activate()
   keys = shortcuts["normal"]
   for _, k in pairs(keys) do
-      log.i(k)
+    log.i(k)
     k:enable()
   end
 end
@@ -68,19 +69,19 @@ end
 -- Returns a string that contains all currently mapped keys
 -- for use in an alert.
 function Keys.getAlertMapping(name)
-    local allKeys = ""
+  local allKeys = ""
 
-    -- TODO(): sort the table for consistent output
-    for appName, mappings in pairs(Keys.triggers) do
-        normalKeys = mappings[1]
-        appKey = normalKeys[2]
-        local modKeys = ""
-        for k, v in pairs(normalKeys[1]) do
-            modKeys = modKeys .. v .. "+"
-        end
-
-        allKeys = allKeys .. string.format("%-32s: %s", appName, modKeys .. appKey) .. "\n"
+  -- TODO(): sort the table for consistent output
+  for appName, mappings in pairs(Keys.triggers) do
+    normalKeys = mappings[1]
+    appKey = normalKeys[2]
+    local modKeys = ""
+    for k, v in pairs(normalKeys[1]) do
+      modKeys = modKeys .. v .. "+"
     end
+
+    allKeys = allKeys .. string.format("%-32s: %s", appName, modKeys .. appKey) .. "\n"
+  end
   return allKeys
 end
 
