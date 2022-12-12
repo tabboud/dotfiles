3dde5dcdae8d1dbb196984de9ab09e07b1aa845f
Tony Abboud <tdabboud@hotmail.com>
2022-12-11 18:43:24 -0800

    vim: Ensure commonly used languages are installed for treesitter

--- vim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ vim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,10 +1,16 @@
 require 'nvim-treesitter.configs'.setup({
   -- list of available parsers
-  ensure_installed = { 'go', 'json', 'yaml' },
+  ensure_installed = {
+    'go',
+    'help',
+    'json',
+    'lua',
+    'vim',
+    'yaml',
+  },
 
   highlight = {
     -- false disables the entire extension
     enable = true,
-    disable = { 'vim' },
   },
 })
