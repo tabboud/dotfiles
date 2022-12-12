8f8a30f3eb4bb88a402ea062e3a0efd46c975a65
Tony Abboud <tdabboud@hotmail.com>
2022-12-11 18:42:25 -0800

    vim: Update first load install path

--- vim/.config/nvim/lua/first_load.lua
+++ vim/.config/nvim/lua/first_load.lua
@@ -3,16 +3,18 @@ local download_packer = function()
     return
   end
 
-  local directory = string.format("%s/site/pack/packer/start/", vim.fn.stdpath "data")
+  local directory = string.format("%s/site/pack/packer/start", vim.fn.stdpath("data"))
   vim.fn.mkdir(directory, "p")
+
+  print "Downloading packer.nvim..."
   local out = vim.fn.system(
     string.format("git clone %s %s", "https://github.com/wbthomason/packer.nvim", directory .. "/packer.nvim")
   )
-
   print(out)
-  print "Downloading packer.nvim..."
-  print "( You'll need to restart now )"
-  vim.cmd [[qa]]
+  vim.cmd [[packadd packer.nvim]]
+  require('plugins')
+  vim.cmd [[PackerSync]]
+  -- vim.cmd [[qa]]
 end
 
 return function()
