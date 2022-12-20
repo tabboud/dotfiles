594120ec0063f8474441a69596f7a02a4ef6a7a9
Tony Abboud <tabboud@palantir.com>
2022-12-20 14:33:28 -0800

    vim: remove old cmp files

--- vim/.config/nvim/lua/plugins/dap.lua
+++ vim/.config/nvim/lua/plugins/dap.lua
@@ -47,13 +47,13 @@ local function configure_exts()
   local dap, dapui = require "dap", require "dapui"
   dapui.setup {} -- use default
   dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
+    dapui.open({})
   end
   dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
+    dapui.close({})
   end
   dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
+    dapui.close({})
   end
 end
 
