955a4d679a8fb0b98bccc334cc7905cb6cf250b6
Tony Abboud <tabboud@palantir.com>
2023-08-01 09:54:50 -0700

    vim: Use todo-comments to highlight all TODOs

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -90,6 +90,23 @@ require('lazy').setup({
     end
   },
   {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("plugins.todo-comments")
+    end,
+    keys = {
+      { "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+      { "<leader>xt", "<cmd>TodoTrouble<cr>",                              desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",      desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",    desc = "Todo/Fix/Fixme" },
+    },
+  },
+  {
     "simrat39/symbols-outline.nvim",
     config = function()
       require("symbols-outline").setup()
