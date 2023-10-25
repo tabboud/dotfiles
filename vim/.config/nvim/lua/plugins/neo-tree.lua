fd4d3c96e332ed4cdbe32a148ba9013390917522
Tony Abboud <tabboud@palantir.com>
2023-10-25 09:47:22 -0700

    vim/neotree: fix filepath copying
    Filepaths were copied to a local register rather than the system
    clipboard.

--- vim/.config/nvim/lua/plugins/neo-tree.lua
+++ vim/.config/nvim/lua/plugins/neo-tree.lua
@@ -29,33 +29,25 @@ require('neo-tree').setup({
         -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
         local node = state.tree:get_node()
         local filepath = node:get_id()
-        local filename = node.name
         local modify = vim.fn.fnamemodify
 
         local results = {
-          filepath,
-          modify(filepath, ':.'),
-          modify(filepath, ':~'),
-          filename,
-          modify(filename, ':r'),
-          modify(filename, ':e'),
+          filepath,               -- absolute path to file (e.g. /Users/user/project/cmd/main.go)
+          modify(filepath, ':.'), -- path relative to CWD, usually the root of a git repo (e.g. cmd/main.go)
         }
 
         -- absolute path to clipboard
         local i = vim.fn.inputlist({
-          'Choose to copy to clipboard:',
-          '1. Absolute path: ' .. results[1],
-          '2. Path relative to CWD: ' .. results[2],
-          '3. Path relative to HOME: ' .. results[3],
-          '4. Filename: ' .. results[4],
-          '5. Filename without extension: ' .. results[5],
-          '6. Extension of the filename: ' .. results[6],
+          'Select a path to copy:',
+          string.format('1. Absolute path: "%s"', results[1]),
+          string.format('2. Project root:  "%s"', results[2]),
         })
 
         if i > 0 then
           local result = results[i]
           if not result then return print('Invalid choice: ' .. i) end
-          vim.fn.setreg('"', result)
+          -- store value into system clipboard register
+          vim.fn.setreg('+', result)
         end
       end
     }
