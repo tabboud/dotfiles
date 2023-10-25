f16489acf233202d4a73d4c6dc322cc7bd9dfef8
Tony Abboud <tabboud@palantir.com>
2023-10-25 09:48:43 -0700

    wezterm: Add fullscreen toggle and update opacity

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,32 +2,29 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
-
--- For example, changing the color scheme:
 config.color_scheme = 'Tomorrow Night Eighties'
 -- config.color_scheme = 'Tomorrow'
 
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.97
+config.window_background_opacity = 0.87
+-- config.macos_window_background_blur = 50 -- blur background windows (useful when opacity is low)
+
 config.audible_bell = "Disabled"
 
--- Make option+{left,right} go backwark/forward words on OSX
 config.keys = {
   -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
   { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
   -- Make Option-Right equivalent to Alt-f; forward-word
   { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
+  -- Toggle FullScreen mode
+  { key = 'Enter',      mods = 'CMD', action = wezterm.action.ToggleFullScreen },
 }
 
 config.window_decorations = "RESIZE"
@@ -49,5 +46,4 @@ config.mouse_bindings = {
   },
 }
 
--- and finally, return the configuration to wezterm
 return config
