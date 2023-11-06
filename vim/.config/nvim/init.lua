61d7bc77044e5d023431e793ba32290735b006d2
Tony Abboud <tabboud@palantir.com>
2023-11-06 10:45:49 -0800

    vim: cache modules if supported

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,5 +1,8 @@
 -- init.lua
 
+-- If supported, compile lua to bytecode (e.g. cache modules).
+if vim.loader and vim.fn.has "nvim-0.9" == 1 then vim.loader.enable() end
+
 -- core
 require("globals")
 require("keymaps")
