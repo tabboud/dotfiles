a72362c5bb53151891b8a43f80c27013041dd040
tabboud <tdabboud@hotmail.com>
2023-11-11 01:49:55 +0000

    vim: move treesitter config to pluginSpec

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -337,7 +337,24 @@ require('lazy').setup({
   {
     'nvim-treesitter/nvim-treesitter',
     config = function()
-      require("plugins.nvim-treesitter")
+      require 'nvim-treesitter.configs'.setup({
+        -- list of available parsers
+        ensure_installed = {
+          'go',
+          'json',
+          'lua',
+          'markdown',
+          'markdown_inline',
+          'rust',
+          'vim',
+          'yaml',
+        },
+
+        highlight = {
+          -- false disables the entire extension
+          enable = true,
+        },
+      })
     end
   },
 
