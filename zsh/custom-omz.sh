a8b6eff8ae9160c24ca8b8821ea7d5180dd791c7
Tony Abboud <tabboud@palantir.com>
2023-12-06 11:26:50 -0800

    zsh: Remove custom zcompdump file naming

--- zsh/custom-omz.sh
+++ zsh/custom-omz.sh
@@ -16,25 +16,15 @@ autoload -U compaudit compinit
 
 # TODO(tabboud): Add all plugins here
 
-# Figure out the SHORT hostname
-if [[ "$OSTYPE" = darwin* ]]; then
-  # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
-  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST=${HOST/.*/}
-else
-  SHORT_HOST=${HOST/.*/}
-fi
-
 # Save the location of the current completion dump file.
 if [ -z "$ZSH_COMPDUMP" ]; then
-  ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+  ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump"
 fi
 compinit -u -C -d "${ZSH_COMPDUMP}"
 
 
 # Load all of library config files
 for config_file ($ZSH/lib/*.zsh); do
-  custom_config_file="${ZSH_CUSTOM}/lib/${config_file:t}"
-  [ -f "${custom_config_file}" ] && config_file=${custom_config_file}
   source $config_file
 done
 
