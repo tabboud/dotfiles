8de451cd907d82c7f4142f066cb61bb0cadd09ea
Tony Abboud <tabboud@palantir.com>
2024-04-01 11:18:08 -0700

    vim: Begin moving to lazy PluginSpecs

--- vim/.config/nvim/lua/config/lazy.lua
+++ vim/.config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.runtimepath:prepend(lazypath)
 
 require('lazy').setup({
   spec = {
-    -- { import = "plugins" },
+    { import = "plugins" },
 
     -----------------
     -- Theme / Tools
@@ -31,7 +31,129 @@ require('lazy').setup({
       'nvim-lualine/lualine.nvim',
       dependencies = { 'nvim-lua/lsp-status.nvim' },
       config = function()
-        require("plugins.lualine")
+        local icons = require("icons").lualine
+        local ok, lsp_status = pcall(require, 'lsp-status')
+        if ok then
+          lsp_status.register_progress()
+        end
+
+        -- projectName returns the name of the containing directory (or project).
+        local project_name = function()
+          local project = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+          return string.format("%s %s", icons.Folder, project)
+        end
+
+        -- get_lsp_status returns the current status of the LSP server.
+        -- If progress messages are available, then they will be shown in real-time, otherwise
+        -- just the currently attached LSP server will be shown or 'none' if none are attached.
+        -- Progress messages are retrieved from https://github.com/nvim-lua/lsp-status.nvim
+        -- via the "lsp-status.status_progress()" method, which is configured to be attached
+        -- in lspconfig's "on_attach" callback.
+        local get_lsp_status = function()
+          for _, client in ipairs(vim.lsp.get_active_clients()) do
+            if client.attached_buffers[vim.api.nvim_get_current_buf()] then
+              local msgs_ok, progress_msg = pcall(function() return require('lsp-status').status_progress() end)
+              if msgs_ok and progress_msg ~= '' then
+                -- show LSP progress message if available
+                return string.format("%s LSP: %s ", icons.Lsp, progress_msg)
+              end
+              -- fallback to just the currently attached LSP server
+              return string.format("%s LSP: %s ", icons.Lsp, client.name)
+            end
+          end
+          -- no clients attached
+          return string.format("%s LSP: none ", icons.Lsp)
+        end
+
+        -- Show the current search count
+        local search_count = function()
+          if vim.v.hlsearch == 0 then
+            return ''
+          end
+
+          local result = vim.fn.searchcount { maxcount = 999, timeout = 500 }
+          local denominator = math.min(result.total, result.maxcount)
+          return string.format('%s [%d/%d]', icons.Search, result.current, denominator)
+        end
+
+        -- Show test results
+        -- TODO: Only load this if neotest is loaded, otherwise this loads neotest/neotest-go
+        local neotest_status = function()
+          local status_ok, neotest = pcall(require, "neotest")
+          if not status_ok then
+            return ""
+          end
+          local adapters = neotest.state.adapter_ids()
+          if #adapters > 0 then
+            local status = neotest.state.status_counts(adapters[1], {
+              buffer = vim.api.nvim_buf_get_name(0),
+            })
+            local sections = {
+              {
+                sign = "",
+                count = status.failed,
+                base = "NeotestFailed",
+                tag = "test_fail",
+              },
+              {
+                sign = "",
+                count = status.running,
+                base = "NeotestRunning",
+                tag = "test_running",
+              },
+              {
+                sign = "",
+                count = status.passed,
+                base = "NeotestPassed",
+                tag = "test_pass",
+              },
+            }
+
+            local result = {}
+            for _, section in ipairs(sections) do
+              if section.count > 0 then
+                table.insert(
+                  result,
+                  "%#"
+                  .. section.base
+                  .. "#"
+                  .. section.sign
+                  .. " "
+                  .. section.count
+                )
+              end
+            end
+
+            return table.concat(result, " ")
+          end
+          return ""
+        end
+
+        require('lualine').setup {
+          options = {
+            theme = IsLightMode() and "onelight" or "jellybeans",
+            component_separators = { left = icons.ComponentSeparator, right = icons.ComponentSeparator },
+            section_separators = { left = '', right = '' },
+          },
+          sections = {
+            lualine_a = { "branch" },
+            lualine_b = { "diff" },
+            lualine_c = { search_count, neotest_status },
+            lualine_x = { get_lsp_status, "diagnostics" },
+            lualine_y = {},
+            lualine_z = { project_name },
+          },
+          inactive_sections = {
+            lualine_a = { "filename" },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {},
+          },
+          tabline = {},
+          extensions = {},
+        }
       end
     },
     {
@@ -59,7 +181,6 @@ require('lazy').setup({
         keymaps.nnoremap("gp", "<cmd>BufferLineCyclePrev<cr>", { desc = "Buffer: Go to prev" })
       end,
     },
-    { import = "plugins.neo-tree" },
     {
       enabled = true,
       "lukas-reineke/indent-blankline.nvim",
@@ -163,12 +284,6 @@ require('lazy').setup({
         { "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
         { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",    desc = "Todo/Fix/Fixme" },
       },
-    },
-    {
-      "stevearc/aerial.nvim",
-      config = function()
-        require('aerial').setup()
-      end
     },
     -- save my last cursor position
     {
@@ -275,7 +390,7 @@ require('lazy').setup({
       'mfussenegger/nvim-dap',
       ft = { 'go' },
       config = function()
-        require("plugins.dap").setup()
+        require("pluginconfig.dap").setup()
       end,
       dependencies = {
         'leoluz/nvim-dap-go',
@@ -294,7 +409,7 @@ require('lazy').setup({
         "nvim-neotest/neotest-go",
       },
       config = function()
-        require("plugins.neotest")
+        require("pluginconfig.neotest")
       end
     },
 
@@ -382,7 +497,7 @@ require('lazy').setup({
     {
       'lewis6991/gitsigns.nvim',
       config = function()
-        require("plugins.gitsigns")
+        require("pluginconfig.gitsigns")
       end
     },
 
@@ -390,8 +505,8 @@ require('lazy').setup({
     -- Languages
     -----------------
     -- Markdown syntax and previewer via glow
-    { 'plasticboy/vim-markdown',  ft = { 'markdown' } },
-    { "rhysd/vim-go-impl",        ft = { 'go' } },
+    { 'plasticboy/vim-markdown', ft = { 'markdown' } },
+    { "rhysd/vim-go-impl",       ft = { 'go' } },
     {
       'MeanderingProgrammer/markdown.nvim',
       name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
@@ -451,7 +566,7 @@ require('lazy').setup({
     {
       'neovim/nvim-lspconfig',
       config = function()
-        require("plugins.lsp")
+        require("pluginconfig.lsp")
       end,
       dependencies = {
         -- Automatically install LSPs/tools to stdpath
@@ -470,8 +585,6 @@ require('lazy').setup({
         "mrded/nvim-lsp-notify"
       },
     },
-    -- Provides a small window to show diagnostics, telescope results, etc.
-    { import = "plugins.trouble" },
     {
       'mrded/nvim-lsp-notify',
       enabled = false,
@@ -499,7 +612,7 @@ require('lazy').setup({
       'hrsh7th/nvim-cmp',
       event = "InsertEnter",
       config = function()
-        require("plugins/cmp")
+        require("pluginconfig.cmp")
       end,
       dependencies = {
         'hrsh7th/cmp-nvim-lsp',
@@ -521,7 +634,7 @@ require('lazy').setup({
     -----------------
     -- Telescope
     -----------------
-    { import = "plugins.telescope" },
+    { import = "pluginconfig.telescope" },
   },
   -- install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
