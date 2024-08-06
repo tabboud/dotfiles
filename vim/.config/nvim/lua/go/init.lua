5d1e345d06f06a48f7b924f12eacc494c04935c4
Tony Abboud <tabboud@palantir.com>
2024-08-06 16:12:18 -0400

    vim: Simply go toggle-test

--- vim/.config/nvim/lua/go/init.lua
+++ vim/.config/nvim/lua/go/init.lua
@@ -4,19 +4,15 @@ local vim = vim
 local fn = vim.fn
 local api = vim.api
 
--- ends_with returns true if str ends with ending.
-local function ends_with(str, ending)
-  return ending == "" or str:sub(- #ending) == ending
-end
-
--- ToggleTest toggles to/from a Go file and the corresponding test.
+---ToggleTest toggles to/from a Go file and the corresponding test.
+---@param vsplit boolean
 function M.ToggleTest(vsplit)
   local alternate_file = ""
 
   local file = api.nvim_buf_get_name(0)
-  if ends_with(file, "_test.go") then
+  if vim.endswith(file, "_test.go") then
     alternate_file = fn.split(file, "_test.go")[1] .. ".go"
-  elseif ends_with(file, ".go") then
+  elseif vim.endswith(file, ".go") then
     alternate_file = fn.split(file, "\\.go")[1] .. "_test.go"
   else
     print("Not a Go file")
