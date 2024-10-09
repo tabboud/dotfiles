659bb2f6e8116500b7f2bb39e0359a51b79f5ef6
Tony Abboud <tabboud@palantir.com>
2024-10-09 10:51:00 -0400

    vim: Add markview.nvim

--- vim/.config/nvim/lua/plugins/languages.lua
+++ vim/.config/nvim/lua/plugins/languages.lua
@@ -2,13 +2,13 @@ return {
   { 'plasticboy/vim-markdown', ft = { 'markdown' } },
   { "rhysd/vim-go-impl",       ft = { 'go' } },
   {
-    'MeanderingProgrammer/markdown.nvim',
-    enabled = true,
-    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    ft = 'markdown',
-    config = function()
-      require('render-markdown').setup({})
-    end,
-  },
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    }
+  }
 }
