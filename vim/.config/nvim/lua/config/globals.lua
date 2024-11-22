8e72a1cb992ffe1283f0309846af2e3a21f74e8a
Tony Abboud <tdabboud@hotmail.com>
2024-11-22 21:52:34 +0000

    Only notify once if theme file is missing

--- vim/.config/nvim/lua/config/globals.lua
+++ vim/.config/nvim/lua/config/globals.lua
@@ -12,9 +12,9 @@ end
 IsLightMode = function()
   local file = io.open(os.getenv("HOME") .. "/.theme", "r")
   if file == nil then
-    vim.notify(
+    vim.notify_once(
       'Error running IsLightMode(). Ensure the file, "$HOME/.theme", exists and contains the current theme ("dark" or "light")',
-      vim.log.levels.ERROR, {})
+      vim.log.levels.WARN, {})
     return false
   end
   local content = file:read("*a")
