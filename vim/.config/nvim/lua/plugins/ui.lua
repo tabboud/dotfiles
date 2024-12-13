b83388f6d4bdcce549c4202b91e0f252a8c9f8aa
Tony Abboud <tabboud@palantir.com>
2024-12-13 16:00:33 -0500

    vim: Fix deprecated API

--- vim/.config/nvim/lua/plugins/ui.lua
+++ vim/.config/nvim/lua/plugins/ui.lua
@@ -12,7 +12,7 @@ return {
 
       -- returns the currently attached LSP server or 'none' if no server is attached.
       local get_attached_lsp = function()
-        for _, client in ipairs(vim.lsp.get_active_clients()) do
+        for _, client in ipairs(vim.lsp.get_clients()) do
           if client.attached_buffers[vim.api.nvim_get_current_buf()] then
             return string.format("%s LSP: %s ", icons.Lsp, client.name)
           end
