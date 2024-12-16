8c382041b908e4aa5ebabc83aa113c80d3edf89c
Tony Abboud <tabboud@palantir.com>
2024-12-16 13:55:16 -0500

    vim: Update luasnip to v2.x

--- vim/.config/nvim/lua/plugins/cmp.lua
+++ vim/.config/nvim/lua/plugins/cmp.lua
@@ -9,7 +9,7 @@ return {
       'saadparwaiz1/cmp_luasnip',
       {
         'L3MON4D3/LuaSnip',
-        version = 'v1.*',
+        version = 'v2.*',
         dependencies = {
           "rafamadriz/friendly-snippets",
         },
