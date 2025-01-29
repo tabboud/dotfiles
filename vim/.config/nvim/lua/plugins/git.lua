6e30bf4ee25e780e4da1dca907f5ac3a6de92a08
Tony Abboud <tdabboud@hotmail.com>
2025-01-29 07:58:00 -0500

    vim: add vgit

--- vim/.config/nvim/lua/plugins/git.lua
+++ vim/.config/nvim/lua/plugins/git.lua
@@ -93,4 +93,13 @@ return {
       end
     }
   },
+  {
+    'tanvirtin/vgit.nvim',
+    branch = 'v1.0.x',
+    -- or               , tag = 'v1.0.2',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+    -- Lazy loading on 'VimEnter' event is necessary.
+    event = 'VimEnter',
+    config = function() require("vgit").setup() end,
+  },
 }
