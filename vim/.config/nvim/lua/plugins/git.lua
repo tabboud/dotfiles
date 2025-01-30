11e771f64b0236ee7a410cabddc7f5c1c4202c8d
Tony Abboud <tabboud@palantir.com>
2025-01-29 19:31:44 -0500

    vim(vgit): disable blame line

--- vim/.config/nvim/lua/plugins/git.lua
+++ vim/.config/nvim/lua/plugins/git.lua
@@ -100,6 +100,14 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
     -- Lazy loading on 'VimEnter' event is necessary.
     event = 'VimEnter',
-    config = function() require("vgit").setup() end,
-  },
+    config = function()
+      require("vgit").setup({
+        settings = {
+          live_blame = {
+            enabled = false,
+          },
+        }
+      })
+    end,
+  }
 }
