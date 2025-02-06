2b05cb14151d1828f45026f981548e82eae50034
Tony Abboud <tdabboud@hotmail.com>
2025-02-06 11:11:56 -0500

    vim: autocmd for relativenumber

--- vim/.config/nvim/lua/config/autocmds.lua
+++ vim/.config/nvim/lua/config/autocmds.lua
@@ -77,3 +77,24 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.cmd(tda_text)
   end
 })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+  pattern = "*",
+  group = dotfiles_group,
+  callback = function()
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+  pattern = "*",
+  group = dotfiles_group,
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      vim.cmd "redraw"
+    end
+  end,
+})
