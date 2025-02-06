149bf6dff3e3e954a034a5a3c95a24148b71449b
Tony Abboud <tabboud@palantir.com>
2025-02-06 11:52:58 -0500

    vim: filter out test go files

--- vim/.config/nvim/lua/plugins/snacks.lua
+++ vim/.config/nvim/lua/plugins/snacks.lua
@@ -157,8 +157,20 @@ return {
         function()
           ---@type snacks.picker.lsp.references.Config
           Snacks.picker.lsp_references({
+            title = "LSP References",
             -- TODO(tabboud):  Add keymaps to toggle test files, mock files, etc.
-            pattern = "!*_test.go",
+            -- pattern = function(picker)
+            --   return "!ingress.go"
+            -- end,
+            ---@type snacks.picker.filter.Config
+            filter = {
+              filter = function(item, filter)
+                if item.file:match("_test.go") or item.file:match("conjure.go") then
+                  return false
+                end
+                return true
+              end
+            },
           })
         end,
         nowait = true,
