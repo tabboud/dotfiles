1413f0d18d204d2de861f3a2827b82d7668a5a85
Tony Abboud <tabboud@palantir.com>
2025-02-13 16:44:18 -0500

    shell: Add function to convert .mov to .gif

--- shell/functions.sh
+++ shell/functions.sh
@@ -290,3 +290,10 @@ hasHelpFlag() {
     done
     return 1
 }
+
+# Convert a .mov file to a .gif
+function movToGif() {
+    # Based on https://gist.github.com/SheldonWangRJT/8d3f44a35c8d1386a396b9b49b43c385
+    output_file="$1.gif"
+    ffmpeg -i $1 -pix_fmt rgb8 -r 10 $output_file && gifsicle -O3 $output_file -o $output_file
+}
