7f7d0ba72ed23ed942863a5e0028e2afbf69e104
Tony Abboud <tabboud@palantir.com>
2025-10-16 11:19:08 -0400

    chore(vim): update plugins

--- vim/.config/nvim/lua/plugins/lazydev.lua
+++ vim/.config/nvim/lua/plugins/lazydev.lua
@@ -1,28 +1,36 @@
 return {
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        { path = "snacks.nvim",        words = { "Snacks" } },
-      },
-    },
-  },
-  -- blink addition to add cmp completion sources for require statements and module annotations
-  {
-    "saghen/blink.cmp",
-    enabled = true,
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev" },
-        providers = {
-          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100 },
-        },
-      },
-    },
-  }
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		dependencies = {
+			{
+				"DrKJeff16/wezterm-types",
+				lazy = true,
+				version = false, -- Get the latest version
+			},
+		},
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				{ path = "snacks.nvim", words = { "Snacks" } },
+				{ path = "wezterm-types", mods = { "wezterm" } },
+			},
+		},
+	},
+	-- blink addition to add cmp completion sources for require statements and module annotations
+	{
+		"saghen/blink.cmp",
+		enabled = true,
+		opts = {
+			sources = {
+				-- add lazydev to your completion providers
+				default = { "lazydev" },
+				providers = {
+					lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100 },
+				},
+			},
+		},
+	},
 }
