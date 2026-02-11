91533577289228d77dc90e6c6a7f5296ddd27a2c
Tony Abboud <tdabboud@hotmail.com>
2026-02-10 20:28:39 -0800

    vim: Enable markview.nvim

--- vim/.config/nvim/lua/plugins/languages.lua
+++ vim/.config/nvim/lua/plugins/languages.lua
@@ -2,7 +2,7 @@ return {
   { 'plasticboy/vim-markdown', ft = { 'markdown' } },
   {
     "OXY2DEV/markview.nvim",
-    enabled = false,
+    enabled = true,
     lazy = false, -- Recommended
     ft = { 'markdown' },
     dependencies = {
